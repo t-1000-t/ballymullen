20e01fef599d0e407b83e334db9ba921f440c389
Vladlen Branetskiy <vbranetskiy@rightandabove.com>
2022-12-14 23:55:58 +0200

    dev
    add alot of new files

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,22 @@
 import React from 'react'
-
 import logo from 'logo.svg'
 import 'App.css'
-import { Header } from "./components"
+import { Header} from './components'
 
 export const App = () => (
-    <div className="App">
-      <Header />
-      <header className="App-header">
-        THIS VERSION REACT-18 FOR CORRECTLY Eslint!
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+  <div className='App'>
+    <Header />
+    <header className='App-header'>
+      THIS VERSION REACT-18 FOR CORRECTLY Eslint!
+      <img src={logo} className='App-logo' alt='logo' />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+        Learn React
+      </a>
+    </header>
+  </div>
+)
 
 export default App
