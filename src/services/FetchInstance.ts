df60cf964895f319f4f6174c7c15d32ed518ae07
Vladlen Branetskiy <vbranetskiy@rightandabove.com>
2023-01-25 21:25:43 +0200

    dev
    Have been added VideoView.tsx.tsx and Fetch for VideoView.tsx.ts

--- src/services/FetchInstance.ts
+++ src/services/FetchInstance.ts
@@ -3,3 +3,9 @@ export const getImages = (query: string, pNum: number) => {
     `https://pixabay.com/api/?key=14147963-1172996dfbc4032cf6219bd1a&q=${query}&page=${pNum}&image_type=photo&pretty=true`,
   ).then((res) => res.json())
 }
+
+export const getVideos = (query: string, pNum: number) => {
+  return fetch(
+    `https://pixabay.com/api/videos/?key=14147963-1172996dfbc4032cf6219bd1a&q=${query}&page=${pNum}&pretty=true`,
+  ).then((res) => res.json())
+}
--- src/views/VideoView.tsx
+++ src/views/VideoView.tsx
@@ -1,5 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getVideos } from '../services/FetchInstance'
+
+interface imagesViewType {
+  total: number
+  totalHits: number
+  hits: []
+}
 
 export const VideoView = () => {
-  return <>VIDEO...</>
+  const [videos, setVideos] = useState({} as imagesViewType)
+
+  useEffect(() => {
+    getVideos('fish', 1).then(setVideos)
+  }, [])
+
+  console.log('videos', videos)
+
+  return (
+    <div className='container'>
+      <div className='row row-cols-1 row-cols-md-2 g-4'>
+        {videos.totalHits &&
+          videos.hits.map((item: any) => {
+            return (
+              <div className='card'>
+                <video src={item.videos.tiny.url} className='object-fit-contain' autoPlay />
+                <div className='card-body'>
+                  <h5 className='card-title'>Card title</h5>
+                  <p className='card-text'>
+                    This is a wider card with supporting text below as a natural lead-in to
+                    additional content. This content is a little bit longer.
+                  </p>
+                  <p className='card-text'>
+                    <small className='text-muted'>Last updated 3 mins ago</small>
+                  </p>
+                </div>
+              </div>
+            )
+          })}
+      </div>
+    </div>
+  )
 }
