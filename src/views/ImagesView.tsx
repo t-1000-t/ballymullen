7aedaac4a678ab3676a12df1f08e33dee7d472a2
Vladlen Branetskiy <vbranetskiy@rightandabove.com>
2023-01-26 00:40:20 +0200

    main
    Have been added Search input

--- src/views/ImagesView.tsx
+++ src/views/ImagesView.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { BaseSyntheticEvent, useEffect, useState } from 'react'
 import { getImages } from '../services/FetchInstance'
 
 interface imagesViewType {
@@ -9,34 +9,67 @@ interface imagesViewType {
 
 export const ImagesView = () => {
   const [images, setImages] = useState({} as imagesViewType)
+  const [textSearch, setTextSearch] = useState('')
+  const [textChange, setTextChange] = useState('')
 
   useEffect(() => {
-    getImages('fish', 1).then(setImages)
-  }, [])
+    getImages(textSearch, 1).then(setImages)
+  }, [textSearch])
 
-  console.log('images', images.hits)
+  const handleSubmit = (event: BaseSyntheticEvent) => {
+    event.preventDefault()
+
+    setTextSearch(textChange)
+  }
+
+  const handleChange = (event: BaseSyntheticEvent) => {
+    event.preventDefault()
+
+    setTextChange(event.currentTarget.value)
+  }
 
   return (
-    <div className='container'>
-      <div className='row row-cols-1 row-cols-md-2 g-4'>
-        {images.totalHits &&
-          images.hits.map((item: any) => {
-            return (
-              <div className='card'>
-                <img src={item.largeImageURL} className='card-img-top' alt='image' />
-                <div className='card-body'>
-                  <h5 className='card-title'>Card title</h5>
-                  <p className='card-text'>
-                    This is a wider card with supporting text below as a natural lead-in to
-                    additional content. This content is a little bit longer.
-                  </p>
-                  <p className='card-text'>
-                    <small className='text-muted'>Last updated 3 mins ago</small>
-                  </p>
+    <div className='album py-5 bg-light'>
+      <div className='container'>
+        <form role='search' className='d-flex p-2'>
+          <div className='col-4 px-2'>
+            <input
+              className='form-control'
+              type='search'
+              placeholder='Search'
+              aria-label='Search'
+              onChange={handleChange}
+              value={textChange}
+            />
+          </div>
+
+          <button className='btn btn-outline-success' type='submit' onSubmit={handleSubmit}>
+            Search
+          </button>
+        </form>
+
+        <div className='row row-cols-1 row-cols-sm-2 row-cols-md-2 g-3'>
+          {images.totalHits &&
+            images.hits.map((item: any) => {
+              return (
+                <div className='col'>
+                  <div className='card p-3 w-40'>
+                    <img src={item.largeImageURL} className='card-img-top' alt={item.type} />
+                    <div className='card-body'>
+                      <h5 className='card-title'>Card title</h5>
+                      <p className='card-text'>
+                        This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.
+                      </p>
+                      <p className='card-text'>
+                        <small className='text-muted'>Last updated 3 mins ago</small>
+                      </p>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            )
-          })}
+              )
+            })}
+        </div>
       </div>
     </div>
   )
--- src/views/VideoView.tsx
+++ src/views/VideoView.tsx
@@ -17,26 +17,45 @@ export const VideoView = () => {
   console.log('videos', videos)
 
   return (
-    <div className='container'>
-      <div className='row row-cols-1 row-cols-md-2 g-4'>
-        {videos.totalHits &&
-          videos.hits.map((item: any) => {
-            return (
-              <div className='card'>
-                <video src={item.videos.tiny.url} className='object-fit-contain' autoPlay />
-                <div className='card-body'>
-                  <h5 className='card-title'>Card title</h5>
-                  <p className='card-text'>
-                    This is a wider card with supporting text below as a natural lead-in to
-                    additional content. This content is a little bit longer.
-                  </p>
-                  <p className='card-text'>
-                    <small className='text-muted'>Last updated 3 mins ago</small>
-                  </p>
+    <div className='album py-5 bg-light'>
+      <div className='container'>
+        <form role='search' className='d-flex p-2'>
+          <div className='col-4 px-2'>
+            <input
+              className='form-control'
+              type='search'
+              placeholder='Search'
+              aria-label='Search'
+            />
+          </div>
+
+          <button className='btn btn-outline-success' type='submit'>
+            Search
+          </button>
+        </form>
+
+        <div className='row row-cols-1 row-cols-sm-2 row-cols-md-2 g-3'>
+          {videos.totalHits &&
+            videos.hits.map((item: any) => {
+              return (
+                <div className='col'>
+                  <div className='card p-3 w-40'>
+                    <video src={item.videos.tiny.url} className='object-fit-contain' autoPlay />
+                    <div className='card-body'>
+                      <h5 className='card-title'>Card title</h5>
+                      <p className='card-text'>
+                        This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.
+                      </p>
+                      <p className='card-text'>
+                        <small className='text-muted'>Last updated 3 mins ago</small>
+                      </p>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            )
-          })}
+              )
+            })}
+        </div>
       </div>
     </div>
   )
